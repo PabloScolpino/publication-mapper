8c7b69f3910ec98bafcdcffbe4806b7d4353af8e
Pablo Scolpino <pabloscolpino@gmail.com>
2016-03-27 18:30:32 +0000

    Removed validation of publication address
    So that publications without addresses can be submitted to the system

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -2,18 +2,6 @@ class Publication < ActiveRecord::Base
   validates :name, presence: true,
                    length: { minimum: 5 }
 
-  validates :country, presence: true,
-                   length: { minimum: 5 }
-
-  validates :province, presence: true,
-                   length: { minimum: 5 }
-
-  validates :locality, presence: true,
-                   length: { minimum: 5 }
-
-  validates :address, presence: true,
-                   length: { minimum: 5 }
-
   scope :approved, -> { where( 'approved_at is not null and approved_at <= ?', DateTime.now() ) }
 
   geocoded_by :full_address
