8c890e2116ee5caa46f473329c3e0ce38ceeb082
Pablo Scolpino <pabloscolpino@gmail.com>
2016-05-30 01:28:47 +0000

    Styled main map with site wide blue tint

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -121,8 +121,14 @@ class PublicationsController < ApplicationController
 
   def grab_map_options
     @map_options = { showTip: true,
-                     mapType: 'normal',
-                     icons: Tag.map_icons
+                     mapType: 'light_dream',
+                     icons: Tag.map_icons,
+                     maps: {
+                       light_dream: {
+                         name: 'Blue Map Style',
+                         styles: [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}]
+                       }
+                     }
                    }
   end
 end
