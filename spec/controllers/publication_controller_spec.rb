6ee6b647ab7f3014b15100bde8f6c16a626085d5
Pablo Scolpino <pabloscolpino@gmail.com>
2020-07-09 18:23:12 -0300

    Setup and first basic spec

---
+++ spec/controllers/publication_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe PublicationsController do
+  describe 'GET map' do
+    let(:publication_1) { create(:publication) }
+    let(:publication_2) { create(:publication) }
+    it 'shows the page' do
+      get :map
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET list of publications' do
+    let(:publication_1) { create(:publication) }
+    let(:publication_2) { create(:publication) }
+    it 'shows the page' do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET show/:id' do
+    let(:publication_1) { create(:publication) }
+    it 'shows the page' do
+      get :show, id: publication_1.id
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
---
+++ spec/factories/publication.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :publication do
+    name { 'location_1' }
+  end
+end
