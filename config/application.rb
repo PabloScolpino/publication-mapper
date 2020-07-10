3a893debadd25170fff1b403adf0b9ea3f1860f7
Pablo Scolpino <pabloscolpino@gmail.com>
2020-07-10 17:02:36 -0300

    Adding missing files from update plus fix depr warn

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module PublicationsMapper
     config.i18n.default_locale = :es
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    #config.active_record.raise_in_transactional_callbacks = true
 
     #attachinary setup
     require 'attachinary/orm/active_record'
