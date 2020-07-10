dc2bc1de44a1e3f0730ee298a3dfc3d5c8678b1e
Pablo Scolpino <itwarp_pscolpino@iseatz.com>
2020-07-10 15:26:50 -0300

    Initial spec coverage for tags controller

---
+++ spec/controllers/tags_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe TagsController do
+  describe 'GET list of publications' do
+    let(:tag_1) { create(:tag) }
+    let(:tag_2) { create(:tag) }
+    it 'shows the page' do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET show/:id' do
+    let(:tag_1) { create(:tag) }
+    it 'shows the page' do
+      get :show, id: tag_1.id
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
